259c770914a1f89cd4b03351292b764ebc401a8a
YeonjiKim0316 <kimyeonji3@gmail.com>
2024-02-14 10:37:03 +0900

    with extends

--- test/views/main_views.py
+++ test/views/main_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from ..models import Question
 
 # 우리가 부를 이름, flask 프레임워크가 찾을 이름, 라우팅주소
@@ -8,9 +8,9 @@ bp = Blueprint('main', __name__, url_prefix="/")
 # @bp.route("/", defaults={"var":'', "var2":""}) # 여러개의 route 어노테이션을 하나의 메서드에 얹어서 쓸 수도 있다
 # @bp.route("/<var>/<var2>")  #대부분 uri는 str로 받기 때문에 str은 생략 # localhost:5000/yeonji  -> hello yeonji가 출력되도록 
 @bp.route("/") 
-def hello(var, var2):
+def index():
 
-    return f'main에서 작성한 Hello {var} {var2}'
+    return render_template("index.html")
 
 @bp.route("/bye")
 def bye():
