ac81900e1ed573bd4f0d51465d91c398bbe633a4
YeonjiKim0316 <kimyeonji3@gmail.com>
2024-02-15 15:32:04 +0900

    add migration

--- test/__init__.py
+++ test/__init__.py
@@ -6,20 +6,32 @@ from flask_sqlalchemy import SQLAlchemy
 # 또는 FLASK_APP이라는 환경변수의 이름을 파일명으로 변경합니다
 # set FLASK_APP=test 
 # wsgi.py에 직접 키=밸류로 여러 환경변수들을 기입합니다.
-
+from sqlalchemy import MetaData
 import config
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 migrate = Migrate()
 
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
 
-def create_app(): # 어플리케이션 팩토리 - 플라스크 서버가 실행될 때 가장 최초로 실행되는 생성자
-    test = Flask(__name__)
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
+
+def create_app():
+    test= Flask(__name__)
+    test.config.from_object(config)
 
     # ORM
-    test.config.from_object(config)
     db.init_app(test)
-    migrate.init_app(test, db)
+    if test.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+        migrate.init_app(test, db, render_as_batch=True)
+    else:
+        migrate.init_app(test, db)
 
     # 블루프린트
     from .views import main_views, board_views, answer_views, auth_views  # views 폴더 및의 main_views.py 임포트
