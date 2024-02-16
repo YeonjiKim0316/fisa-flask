5ff6ab62dbada284ccc2733fe94dc443b92082c8
YeonjiKim0316 <kimyeonji3@gmail.com>
2024-02-16 10:51:42 +0900

    add Dockerfile

--- test/models.py
+++ test/models.py
@@ -15,7 +15,7 @@ class Question(db.Model):
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
-    question = db.relationship('Question', backref=db.backref('answer_set'))
+    question = db.relationship('Question', backref=db.backref('answer_set', cascade="delete"))
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
     # user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=True, server_default="1")
--- test/views/answer_views.py
+++ test/views/answer_views.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, redirect, render_template, url_for, g 
+from flask import Blueprint, redirect, render_template, url_for, g, flash, request
 from ..forms import AnswerForm
 from test import db
 from datetime import datetime
 from ..models import Answer
+from test.views.auth_views import login_required
 
 answer = Blueprint('answer', __name__, url_prefix="/answer")
 
@@ -23,3 +24,46 @@ def create(question_id):
         return redirect (url_for ('board.post_detail', question_id=question_id))
     # 빈 화면으로 넘기기
     return render_template('question/question_detail.html', question_id=question_id, form=form)
+
+@answer.route("/modify/<int:answer_id>", methods=('GET', 'POST'))
+@login_required
+def modify(answer_id):
+    # 답변을 가져온다
+    answer = Answer.query.get_or_404(answer_id)
+    question_id = answer.question_id
+    # 지금 답변을 변경하려는 사람(로그인한사람)이 작성자인지 확인한다
+    if g.user != answer.user: 
+        flash('수정권한이 없습니다')
+        return redirect(url_for('board.post_detail', question_id=question_id))
+    # 아니면 flash로 에러메시지 전달
+    # 맞으면, post로 값이 왔으면
+    if request.method == 'POST':
+        form =AnswerForm()
+        if form.validate_on_submit():
+            form.populate_obj(answer) # 화면에 원래 db에서 꺼낸 값을 변경해서 뿌림
+            db.session.commit()
+            return redirect(url_for('board.post_detail', question_id=question_id))
+            # 값을 수정하여 다시 session에 commit
+    else: # GET으로 요청이 왔을 때
+        form = AnswerForm(obj=answer)
+    return render_template('answer/answer_form.html', form=form, question_id=question_id)
+    # 원래 화면으로 redirect
+
+
+@answer.route("/delete/<int:answer_id>")
+@login_required
+def delete(answer_id):
+    # 글을 가져옴
+    answer = Answer.query.get_or_404(answer_id)
+    # 현재 접속한 사용자와 글의 작성자가 일치하는지 확인
+    question_id= answer.question_id
+    if g.user != answer.user: 
+        flash('댓글 삭제 권한이 없습니다')
+    #     일치하지 않으면 -> 삭제권한이 없습니다 메시지 출력
+        return redirect(url_for('board.post_detail', question_id=question_id))
+    #     원래 글로 되돌아감
+    db.session.delete(answer)
+    db.session.commit()
+    # 커밋
+    # question_list로 되돌아감 
+    return redirect(url_for('board.post_detail', question_id=question_id))
